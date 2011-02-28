42b5402d475733067a3129ee04485479cba7b21f
Najaf Ali <ali.najaf@gmail.com>
2011-02-28 16:57:20 -0500

    added seed content sql

---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+INSERT INTO pages (title, content, meta_description, meta_keywords, uri) VALUES 
+('About Us', '<p>Aliquam aliquet, est a ullamcorper condimentum, tellus nulla fringilla elit, a iaculis nulla turpis sed wisi. Fusce volutpat. Etiam sodales ante id nunc. Proin ornare dignissim lacus. Nunc porttitor nunc a sem. Sed sollicitudin velit eu magna. Aliquam erat volutpat. Vivamus ornare est non wisi. Proin vel quam. Vivamus egestas. Nunc tempor diam vehicula mauris. Nullam sapien eros, facilisis vel, eleifend non, auctor dapibus, pede.</p>', 'A page about the website', 'a, page, about, the, website', 'about.html'),
+
+('Contact', '<p>Contact Name and Email</p>', 'Use this page to get in touch', 'the, contact, page', 'contact.html'),
+
+('Gallery', '<p>Add photos</p>', 'Add photos here', 'page, for, photos', 'gallery.html'),
+
+('Freshly squeezed lorem ipsum text', '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum <a href=\'#\'>sed pharetra</a> gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. <a href=\'#\'>Nulla facilisi</a>. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis.</p>
+
+<img src=\'images/st-pauls.jpg\' />
+    
+<p>Nam viverra sapien eget <strong>augue bibendum eleifend</strong> iaculis tortor blandit. In tortor nunc, venenatis ac facilisis quis, ullamcorper vitae lacus. Proin auctor, sem eget ultrices tempus, massa libero auctor enim, ac lobortis ante eros in orci. Mauris dictum lorem nec enim iaculis nec pharetra odio ultricies. Quisque molestie diam id velit aliquet tempus. Donec varius volutpat dolor in tristique. Nulla ornare volutpat ipsum sit amet blandit. Curabitur quis aliquet purus. Fusce at turpis purus, vel pellentesque risus. Duis bibendum diam eget neque venenatis molestie.</p>
+
+<h3>Lorem ipsum</h3>
+
+<ul>
+  <li>A</li>
+  <li>List</li>
+  <li>Of Interesting</li>
+  <li>Things</li>
+</ul>
+
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum <a href=\'#\'>sed pharetra</a> gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. <a href=\'#\'>Nulla facilisi</a>. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis.</p>', 'the index page', 'index,page,for,site', 'index.html');
+
