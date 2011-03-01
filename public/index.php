290249e7171151da3779377e729fa356d7e1a207
Geetha Ramachandran <geetha_ramachandran@ymail.com>
2011-03-01 18:58:03 +0000

    changed function name

--- public/index.php
+++ public/index.php
@@ -12,15 +12,17 @@ $view = new View();
 $uri = $_SERVER['REQUEST_URI'];
 
 $templateFilename = '../site/view/pages/' . substr($uri, 1) . '.php';
-//
-//$view->setLayoutFilename('../site/view/layouts/default.html.php');
-//
-//if (is_readable($templateFilename)) {
-//    $view->setTemplateFilename($templateFilename);
-//} else {
+
+$view->setLayoutFilename('../site/view/layouts/default.html.php');
+
+if (is_readable($templateFilename)) {
+    $view->setTemplateFilename($templateFilename);
+} 
+
+//else {
 //    header("HTTP/1.0 404 Not Found");
 //    $view->setTemplateFilename('../site/view/pages/404.html.php');
 //}
-//    $view->render();
+    $view->renderLayout();
     
 ?>
