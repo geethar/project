f1ccf38f93de6e68f0bdb7630e17534672dce5aa
Geetha Ramachandran <geetha_ramachandran@ymail.com>
2011-03-03 11:29:43 +0000

    added some echo statements

--- public/index.php
+++ public/index.php
@@ -9,13 +9,14 @@ require '../lib/view.php';
 
 $view = new View();
 
+echo "The following is a dump of the view object:";
+var_dump($view); echo "<br /><br />";
+
 $uri = $_SERVER['REQUEST_URI'];
 
 $templateFilename = '../site/view/pages/' . substr($uri, 1) . '.php';
 
-$view->setLayoutFilename('../site/view/layouts/default.html.php');
-
-echo $view->LayoutFilename;
+echo "templateFilename is " . $templateFilename . "<br /><br />";
 
 if (is_readable($templateFilename)) {
     $view->setTemplateFilename($templateFilename);
@@ -25,6 +26,12 @@ if (is_readable($templateFilename)) {
 //    header("HTTP/1.0 404 Not Found");
 //    $view->setTemplateFilename('../site/view/pages/404.html.php');
 //}
-    $view->renderLayout();
+
+$view->setLayoutFilename('../site/view/layouts/default.html.php');
+
+$layoutname_check = $view->getLayoutFilename();
+echo "layoutFilename is" . $layoutname_check . "<br /><br />";
+
+$view->renderLayout();
     
 ?>
