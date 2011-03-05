eef80fd316106c639c037f42c24bf68a92990deb
Geetha Ramachandran <geetha_ramachandran@ymail.com>
2011-03-05 15:19:12 +0000

    echo commented out, and 404 stuff added

--- public/index.php
+++ public/index.php
@@ -9,28 +9,28 @@ require '../lib/view.php';
 
 $view = new View();
 
-echo "The following is a dump of the view object:";
-var_dump($view); echo "<br /><br />";
+//echo "The following is a dump of the view object:";
+//var_dump($view); echo "<br /><br />";
 
 $uri = $_SERVER['REQUEST_URI'];
 
 $templateFilename = '../site/view/pages/' . substr($uri, 1) . '.php';
 
-echo "templateFilename is " . $templateFilename . "<br /><br />";
+//echo "templateFilename is " . $templateFilename . "<br /><br />";
 
 if (is_readable($templateFilename)) {
     $view->setTemplateFilename($templateFilename);
 } 
 
-//else {
-//    header("HTTP/1.0 404 Not Found");
-//    $view->setTemplateFilename('../site/view/pages/404.html.php');
-//}
+else {
+    header("HTTP/1.0 404 Not Found");
+    $view->setTemplateFilename('../site/view/pages/404.html.php');
+}
 
 $view->setLayoutFilename('../site/view/layouts/default.html.php');
 
-$layoutname_check = $view->getLayoutFilename();
-echo "layoutFilename is" . $layoutname_check . "<br /><br />";
+//$layoutname_check = $view->getLayoutFilename();
+//echo "layoutFilename is" . $layoutname_check . "<br /><br />";
 
 $view->renderLayout();
     
---
+++ site/view/pages/404.html.php
@@ -0,0 +1,3 @@
+<h2>404: Oh No!</h2>
+
+<p>The file you are looking for doesn't exist.</p>
